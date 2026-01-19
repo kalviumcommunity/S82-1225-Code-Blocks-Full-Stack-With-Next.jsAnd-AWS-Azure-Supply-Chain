ee0dc573222571028053ba3b14fbe3b28b80f414
hrishob <hrishobp@gmail.com>
2026-01-19 11:53:13 +0530

    Did the zod

--- app/api/tasks/route.ts
+++ app/api/tasks/route.ts
@@ -1,16 +1,28 @@
+import { taskSchema } from "@/lib/schemas/taskSchema";
 import { sendSuccess, sendError } from "@/lib/responseHandler";
 import { ERROR_CODES } from "@/lib/errorCodes";
+import { ZodError } from "zod";
 
-export async function POST() {
+export async function PUT(req: Request) {
   try {
-    const task = { id: 101, title: "Learn Next.js API Handlers" };
-    return sendSuccess(task, "Task created successfully", 201);
-  } catch (err) {
-    return sendError(
-      "Task creation failed",
-      ERROR_CODES.DATABASE_FAILURE,
-      500,
-      err
-    );
+    const body = await req.json();
+    const validatedData = taskSchema.parse(body);
+
+    return sendSuccess(validatedData, "Task updated successfully", 200);
+
+  } catch (error) {
+    if (error instanceof ZodError) {
+      return sendError(
+        "Validation Error",
+        ERROR_CODES.VALIDATION_ERROR,
+        400,
+        error.issues.map((e) => ({
+          field: e.path[0],
+          message: e.message,
+        }))
+      );
+    }
+
+    return sendError("Internal Error", ERROR_CODES.INTERNAL_ERROR, 500);
   }
 }
